212b2eb98232f784d9f6229975a42a445a250e45
Romesh Abeyawardena <romesh.a@outlook.com>
2025-09-02 21:45:36 +0100

    Added files

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
-import './assets/main.css'
+import './assets/main.css';
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import { createPinia } from 'pinia';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+const pinia = createPinia();
 
 const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            prefix: 'p',
+            darkModeSelector: 'system'
+        }
+    }
+});
+app.use(pinia);
+app.use(router);
+app.mount('#app');
